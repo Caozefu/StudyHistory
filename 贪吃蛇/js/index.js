4f54c3fa7abb5f72683705b672fe77c12e001c10
Luminous <316149689@qq.com>
2018-06-03 01:49:18 +0800

    贪吃蛇

--- 贪吃蛇/js/index.js
+++ 贪吃蛇/js/index.js
@@ -1,16 +1,26 @@
-window.onload = function() {
-    // 设置CSS样式方法
-    function setCSS (obj,json) {
-        for(attr in json) {
-            obj.style[attr] = json[attr];
-        }
-    }
-    // 生成随机数方法
-    function randomNumber(min,max) {
-        return min + Math.floor(Math.random() * (max - min + 1));
-    }
+window.onload = function () {
+    var map = document.getElementById("map");
 
-    (function() {
+    // 封装用到的方法
+    (function () {
+        // 设置CSS样式方法
+        function setCSS(obj, json) {
+            for (attr in json) {
+                obj.style[attr] = json[attr];
+            }
+        }
+
+        // 生成随机数方法
+        function randomNumber(min, max) {
+            return min + Math.floor(Math.random() * (max - min + 1));
+        }
+
+        window.setCSS = setCSS;
+        window.randomNumber = randomNumber;
+    })();
+
+    // 食物
+    (function () {
         // 创建食物对象
         function Food(color) {
             this.width = 20;
@@ -19,9 +29,10 @@ window.onload = function() {
             this.element = document.createElement('div');
             this.init();
         }
+
         // 初始化食物属性
-        Food.prototype.init = function() {
-            setCSS(this.element,{
+        Food.prototype.init = function () {
+            setCSS(this.element, {
                 "width": this.width + "px",
                 "height": this.height + "px",
                 "backgroundColor": this.color,
@@ -30,29 +41,176 @@ window.onload = function() {
             this.create();
         };
         // 食物随机生成
-        Food.prototype.create = function() {
-            setCSS(this.element,{
-                "left": randomNumber(0,49) * this.width + "px",
-                "top": randomNumber(0,39) * this.height + "px"
+        Food.prototype.create = function () {
+            setCSS(this.element, {
+                "left": randomNumber(0, 49) * this.width + "px",
+                "top": randomNumber(0, 39) * this.height + "px"
             });
-            document.getElementById("map").appendChild(this.element);
+            map.appendChild(this.element);
         };
-        Food.prototype.remove = function() {
+        Food.prototype.remove = function () {
             this.element.parentNode.removeChild(this.element);
         };
 
         window.Food = Food;
     })();
 
-
-    (function() {
-        // 实例化食物对象
-        var food = new Food("green");
-
-        document.getElementById("btn2").onclick = function() {
-            food.remove();
-        };
+    // 小蛇
+    (function () {
         // 创建小蛇对象
+        function Snake(width, height, direction) {
+            this.width = width || 20;
+            this.height = height || 20;
+            this.direction = direction || "right";
+            this.elements = [];
+            this.body = [
+                {left: 2, top: 2, color: "blue"},//头
+                {left: 1, top: 2, color: "yellow"},//身体。。。
+                {left: 0, top: 2, color: "yellow"}
+            ];
+            this.init();
+            this.run();
+        }
 
+        // 初始化小蛇
+        Snake.prototype.init = function () {
+            this.remove();
+
+            for (var i = 0; i < this.body.length; i++) {
+                var b = this.body[i],
+                    div = document.createElement("div")
+                ;
+                setCSS(div, {
+                    "width": this.width + "px",
+                    "height": this.height + "px",
+                    "position": "absolute",
+                    "backgroundColor": b.color,
+                    "left": b.left * this.width + "px",
+                    "top": b.top * this.height + "px"
+                });
+                map.appendChild(div);
+
+
+                //加入elements
+                this.elements.push(div);
+            }
+        };
+        // 小蛇运动
+        Snake.prototype.run = function () {
+            var that = this,
+                timer
+            ;
+            var timer = setInterval(function () {
+                /*document.onkeydown = function (event) {
+                        var keyNum;
+                        if (window.event) {
+                            keyNum = event.keyCode;
+                        } else {
+                            keyNum = event.which;
+                        }
+                        if (keyNum === 39) {
+                            clearInterval(timer);
+                            let target = that.body[0].top;
+                            timer = setInterval(function() {
+                                for (let i = 0; i < that.body.length; i++) {
+                                    if (that.body[i].top === target) {
+                                        that.body[i].left++;
+                                    } else {
+                                        that.body[i].top++;
+                                    }
+                                    if(that.body[i].left === 49) {
+                                        clearInterval(timer);
+                                    }
+                                }
+                                that.init();
+                            },300);
+
+                        } else if (keyNum === 40) {
+                            clearInterval(timer);
+                            let target = that.body[0].left;
+                            timer = setInterval(function() {
+                                for (let i = 0; i < that.body.length; i++) {
+                                    if (that.body[i].left === target) {
+                                        that.body[i].top++;
+                                    } else {
+                                        that.body[i].left++;
+                                    }
+                                }
+                                that.init();
+                            },300);
+
+                        }
+                    };*/
+                document.onkeydown = function (event) {
+                    event.preventDefault();
+                    var keyNum;
+                    if (window.event) {
+                        keyNum = event.keyCode;
+                    } else {
+                        keyNum = event.which;
+                    }
+                    if (keyNum === 39) {
+                        if (that.direction !== "left") {
+                            that.direction = "right";
+                        }
+                    } else if (keyNum === 40) {
+                        if (that.direction !== "top") {
+                            that.direction = "bottom";
+                        }
+                    } else if (keyNum === 37) {
+                        if (that.direction !== "right") {
+                            that.direction = "left";
+                        }
+                    } else if (keyNum === 38) {
+                        if (that.direction !== "bottom") {
+                            that.direction = "top";
+                        }
+                    }
+                };
+                for (let i = that.body.length - 1; i > 0; i--) {
+                    that.body[i].left = that.body[i - 1].left;
+                    that.body[i].top = that.body[i - 1].top;
+                }
+                switch (that.direction) {
+                    case "right":
+                        that.body[0].left++;
+                        break;
+                    case "left":
+                        that.body[0].left--;
+                        break;
+                    case "top":
+                        that.body[0].top--;
+                        break;
+                    case "bottom" :
+                        that.body[0].top++;
+                        break;
+                }
+                // if (that.direction === "right") {
+                //     that.body[0].left++;
+                // } else if (that.direction === "left") {
+                //     that.body[0].left--;
+                // } else if (that.direction === "top") {
+                //     that.body[0].top--;
+                // } else if (that.direction === "bottom") {
+                //     that.body[0].top++;
+                // }
+
+                that.init();
+            }, 300);
+        };
+
+        Snake.prototype.remove = function () {
+            //debugger;
+            for (let i = 0; i < this.elements.length; i++) {
+                this.elements[i].parentNode.removeChild(this.elements[i]);
+            }
+            this.elements.splice(0, this.body.length);
+        };
+
+        window.Snake = Snake;
     })();
+
+    // 实例化食物对象
+    var food = new Food("green");
+    var snake = new Snake(20, 20, "right");
 };
